39f98437c2118c2a69b549040debebd9dab33711
nardisync <nardi.sync@gmail.com>
2023-05-03 19:50:20 +0200

    Ending the developemnt of the capstone

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
+    'rest_framework',
+    'djoser',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    
+}
+
+DJOSER = {
+    "USER_ID_FIELD":"username"
+    }
+
+DEFAULT_RENDERER_CLASSES = [
+    'rest_framework.renderers.BrowsableAPIRenderer'
+]
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,23 @@
 
 from django.contrib import admin
-from django.urls import path
-from .views import sayHello, index
+from django.urls import path, include
+from .views import sayHello, index, MenuItemsView, SingleMenuItemView, BookingViewSet
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+router = routers.DefaultRouter()
+router.register(r'tables', BookingViewSet)
+
 
 urlpatterns = [
     #path('', sayHello, name='sayHello'),
-    path('', index, name='index')
+    path('', include(router.urls)),
+    #path('', index, name='index'),
+    path('api-token-auth/', obtain_auth_token),
+    # ---------------------------------------------------------
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    # ---------------------------------------------------------
+    path('booking/', include(router.urls)),
 ]
